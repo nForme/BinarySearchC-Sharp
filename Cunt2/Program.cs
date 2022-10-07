bb5b350264cee0d1e252157147df6137521f61b3
nForme <narik229223@mail.ru>
2022-10-07 11:19:54 +0500

    Добавьте файлы проекта.

---
+++ Cunt2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using Cunt2;
+
+class Progam
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter an integer value between 1 and 20:");
+        try
+        {
+            //user input
+            string input = Console.ReadLine();
+            int value_of_input = int.Parse(input);
+            //lowest random integer
+            int min = 0;
+            //maximum random integer
+            int max = 20;
+            int[] binary_search = new int[value_of_input];
+            //Generate a random number instance called randNum
+            Random randNum = new Random();
+
+            //Populate the array with random numbers between 0 and 20
+            for (int i = 0; i < value_of_input; i++)
+            {
+                binary_search[i] = randNum.Next(min, max);
+                Console.Write(" " + binary_search[i]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //Here user should enter an integer value from the list displayed by the array in the console
+            Console.Write("Now enter one integer value from this array to conduct the binary search");
+
+            string get_search = Console.ReadLine();
+            int value_of_get_search = int.Parse(get_search);
+
+            //Now sort the integer values in the array as required by binary search
+            Array.Sort(binary_search);
+            Console.WriteLine("");
+            Console.WriteLine("Here is the array with the integer values in order as required by binary search");
+            Console.WriteLine("");
+
+            //Print the ordered array to the console
+            for (int i = 0; i < value_of_input; i++)
+            {
+                Console.Write("  " + binary_search[i]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Now lets search for you integer value ....");
+            //get middle will be used to get the element in the middle of the array
+            int get_middle = 0;
+            //lower element 
+            int low = 0;
+            //This is the upper element
+            int high = (binary_search.Length) - 1;
+            //This is the middle if the array. mid is rounded automatically if (low + high)
+            int mid = (low + high) / 2;
+            int track_middle = 0;
+
+            //Here binary search Algorithm
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                //Get the middle element in the binary_search array
+                get_middle = binary_search[mid];
+
+                if (get_middle == value_of_get_search)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Found your integer! Here it is " + value_of_get_search);
+                    break;
+                }
+
+                if (get_middle > value_of_get_search && get_middle != track_middle)
+                {
+                    high = mid + 1;
+                    Console.WriteLine("");
+                    Console.WriteLine("Found this integer:" + get_middle + "But thats not it!");
+                    track_middle = get_middle;
+                }
+
+                //If no match is found, low will continue to increase I more utill it exceeds high
+                //Otherwise low will be used to contonue the search
+                else
+                {
+                    low = mid + 1;
+                    Console.WriteLine("");
+                    Console.WriteLine("Found this integer" + get_middle + "But thats not it");
+                }
+                if (low > high)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Could not locate your integer value in the search. Pls try again");
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to continue .....");
+                Console.ReadKey(true);
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Please enter an integer value and try again .....");
+            Console.ReadKey(true);
+        }
+
+        Utilities.CompareTimes();
+    }
+}
---
+++ Cunt2/Utilities.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Cunt2
+{
+    class Utilities
+    {
+        public static void CompareTimes()
+        {
+            try
+            {
+                //Utility objects
+                Stopwatch watch = Stopwatch.StartNew();
+                Random randNum = new Random();
+
+                //Nuber to search
+                int value_of_get_search = 198023;
+
+                //Data collection
+                int size_of_input = 100000000;
+                int[] binary_search = new int[size_of_input];
+
+                //Number range for the binary_search array
+                int min = 0;
+                int max = 200000;
+
+                //Populate the data collection
+                for (int i = 0; i < size_of_input; i++)
+                {
+                    binary_search[i] = randNum.Next(min, max);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Lets now times for the two methods");
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                //simple search
+                int searchIndex = 0;
+                bool simpleSearchFound = false;
+                watch.Restart();
+
+                while (!simpleSearchFound && searchIndex < size_of_input)
+                {
+                    if (binary_search[searchIndex] == value_of_get_search)
+                    {
+                        simpleSearchFound = true;
+                    }
+                    else
+                    {
+                        searchIndex++;
+                        Console.WriteLine("Searching at: " + searchIndex);
+                    }
+                }
+                watch.Stop();
+                long elapsedMs = watch.ElapsedMilliseconds;
+                Console.WriteLine("Simple search took" + elapsedMs + "mSecs and" + searchIndex + "comparisons");
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                Array.Sort(binary_search);
+                Console.WriteLine("");
+                Console.WriteLine("Collection sort completed as required by Binary Serch");
+                Console.WriteLine("");
+
+                Console.WriteLine("");
+                Console.WriteLine("Now lets search for your integer value ...");
+
+                //binary search indices
+                int get_middle = 0;
+                int low = 0;
+                int high = (binary_search.Length) - 1;
+                int mid = (low + high) / 2;
+                int track_middle = 0;
+
+                //Here is the Binary Search Algorithm
+                int searchCounter = 0;
+                watch.Restart();
+
+                while (low <= high)
+                {
+                    searchCounter++;
+
+                    mid = (low + high) / 2;
+                    get_middle = binary_search[mid];
+                    if (get_middle == value_of_get_search)
+                    {
+                        break;
+                    }
+
+                    if (get_middle > value_of_get_search && get_middle != track_middle)
+                    {
+                        high = mid + 1;
+                        track_middle = get_middle;
+                    }
+
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+
+                if (low > high)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Could not locate your integer value int the search. Please try again...");
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                watch.Stop();
+                elapsedMs = watch.ElapsedMilliseconds;
+
+                Console.WriteLine("Binary search took took " + elapsedMs + "mSecs and " + searchCounter + " comparisons. Found item at location " + mid);
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to exit the programm ....");
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value and try again ....");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
